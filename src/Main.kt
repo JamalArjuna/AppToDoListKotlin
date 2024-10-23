8a64341dfd1df45f6e57d998df8fb5346dcb6a00
JamalArjuna <arjunaridwansyah@gmail.com>
2024-10-23 19:50:12 +0700

    Making a Function Show To Do List with Unit Test

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import java.util.Scanner;
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+val activity: Array<String?> = arrayOfNulls(10)
+val inputUser = Scanner(System.`in`)
+//Show To Do List
+fun ShowToDoList(todo: String) {
+    println("===== TO DO LIST HARI INI ======")
+    for (i in activity.indices){
+        if (i == null){
+            return
+        }else{
+            println("${i + 1}. $todo")
+            break
+        }
     }
+}
+
+fun TestShowToDoList() {
+    val aktivitas = "Belajar"
+
+    ShowToDoList(aktivitas)
+
+}
+
+fun AddToDoList() {
+
+}
+
+fun EditToDoList() {
+
+}
+
+fun RemoveToDoList() {
+
+}
+
+fun UIAddToDoList() {
+
+}
+
+fun UIEditToDoList() {
+
+}
+
+fun UIRemoveToDoList() {
+
+}
+    fun main() {
+        // MAIN PROGRAM
+        TestShowToDoList()
 }
