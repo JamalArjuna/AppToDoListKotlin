a6e5a6645138085113bfc936222bbfa442b13325
JamalArjuna <arjunaridwansyah@gmail.com>
2024-10-23 21:03:40 +0700

    Making a Function Add To Do List with Unit Test

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,67 @@
 import java.util.Scanner;
 
-val activity: Array<String?> = arrayOfNulls(10)
+var activity: Array<String?> = arrayOfNulls(10)
 val inputUser = Scanner(System.`in`)
 //Show To Do List
-fun ShowToDoList(todo: String) {
+fun ShowToDoList() {
     println("===== TO DO LIST HARI INI ======")
-    for (i in activity.indices){
-        if (i == null){
-            return
-        }else{
+    for (i in activity.indices) {
+        var todo = activity[i]
+        if (todo != null){
             println("${i + 1}. $todo")
+        }else{
             break
         }
     }
 }
 
 fun TestShowToDoList() {
-    val aktivitas = "Belajar"
-
-    ShowToDoList(aktivitas)
+    AddToDoList("Makan")
+    AddToDoList("Makan")
+    AddToDoList("Makan")
+    ShowToDoList()
 
 }
 
-fun AddToDoList() {
+fun AddToDoList(todo: String) {
+    var isFull = true
+    var temp: String?
+    // Function Pengecekan apakah Array Masih NULL atau tidak
+    for (i in activity.indices){
+        if (i == null){
+            isFull = false
+            break
+        }
+    }
+    // Jika penuh kita resize
+    if (isFull){
+        activity = activity.copyOf(activity.size * 2)
+    }
+    // Menyimpan Array ke variabel sementara
+    temp = activity[activity.size-1]
+    // Mengganti array sementara dengan Array Permanen
+    if (temp != null) {
+        for (i in temp.indices){
+            temp = activity[i]
+            break
+        }
+    }
 
+    // Menambahkan To Do ke data null
+    for (i in activity.indices){
+        if (activity[i] == null){
+            activity[i] = todo
+            break
+        }
+    }
+}
+
+fun TestAddToDoList() {
+    AddToDoList("MAKAN")
+    AddToDoList("MAKAN")
+    AddToDoList("MAKAN")
+    AddToDoList("MAKAN")
+    AddToDoList("MAKAN")
 }
 
 fun EditToDoList() {
@@ -45,7 +83,8 @@ fun UIEditToDoList() {
 fun UIRemoveToDoList() {
 
 }
-    fun main() {
-        // MAIN PROGRAM
-        TestShowToDoList()
+fun main() {
+    // MAIN PROGRAM
+    TestShowToDoList()
+    // TestAddToDoList()
 }
