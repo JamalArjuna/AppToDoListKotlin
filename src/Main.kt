75e60572f4c4c99913e1ec624614cb0bc48a504a
JamalArjuna <arjunaridwansyah@gmail.com>
2024-10-27 12:14:37 +0700

    Final APP. Release 1.0

--- src/Main.kt
+++ src/Main.kt
@@ -13,6 +13,9 @@ fun ShowToDoList() {
             break
         }
     }
+    if (activity.first() == null && activity.last() == null){
+        println("Sepertinya Kamu Belum Punya Aktivitas Hari Ini, Tambahkan Beberapa Aktivitas")
+    }
 }
 
 fun TestShowToDoList() {
@@ -66,22 +69,26 @@ fun TestAddToDoList() {
 
 fun RemoveToDoList(inputNumber: Int): Boolean {
     val Angka = inputNumber - 1
+    // Pengecekan apakah Input Angka kurang dari 0 dan lebih dari size index
     if (Angka <= -1 && Angka > activity.size){
         return false
+    // Pengecekan apakah Input null
     }else if(activity[Angka] == null){
         return false
     } else{
+        // Looping
         for (i in activity.indices){
+            // Pengecekan apakah input mendekati 0
             if (i == activity.size - 1){
                 activity[i] = null
-
             }
+            // Penggeseran penomoran
             else activity[i] = activity[i + 1]
         }
     }
     return true
 }
-
+// Unit TEST
 fun TestRemoveToDoList(){
     AddToDoList("Makan")
     AddToDoList("Main")
@@ -90,24 +97,144 @@ fun TestRemoveToDoList(){
     RemoveToDoList(1)
     ShowToDoList()
 }
-fun EditToDoList() {
+fun EditToDoList(inputNumber: Int, inputToDo: String): Boolean {
+    val Angka = inputNumber - 1
+    if (Angka > activity.size){
+        return false
+    } else if(Angka == null){
+        return false
+    }else{
+        activity[Angka] = inputToDo
+    }
+return true
+}
+fun TestEditToDoList() {
+    AddToDoList("Makan")
+    AddToDoList("Kerja")
+    AddToDoList("Mandi")
+    AddToDoList("Rebahan")
+    ShowToDoList()
+    EditToDoList(1, "Tidur")
+    ShowToDoList()
 
 }
 
 fun UIAddToDoList() {
 
+    println("\n========= TAMBAHKAN TO DO LIST KAMU HARI INI =========")
+
+    print("Masukkan To Do List: ")
+    val todo = inputUser.nextLine()
+    if (todo == null){
+        println("Input Kamu Tidak Sesuai, Silahkan isi To Do dengan teliti!!")
+        return
+    } else{
+        AddToDoList(todo)
+    }
+}
+
+fun TestUIAddToDoList() {
+    UIAddToDoList()
+    ShowToDoList()
 }
 
 fun UIEditToDoList() {
+    println("\n========= EDIT TO DO LIST KAMU HARI INI =========")
+
+    print("Masukkan To Do List: ")
+    val todo = inputUser.nextLine()
+    print("Masukkan To Do List Keberapa Yang Mau Diganti: ")
+    val Num = inputUser.nextInt()
+
+    if (todo == null){
+        println("Input Kamu Tidak Sesuai, Silahkan isi To Do dengan teliti!!")
+        return
+    }else if (Num == null){
+        println("Input Kamu Tidak Sesuai, Silahkan isi Nomor To Do List yang ingin diganti dengan teliti!!")
+        return
+    }
+    else{
+        EditToDoList(Num, todo)
+    }
+}
+
+fun TestUIEditToDoList() {
+    AddToDoList("Makan")
+    AddToDoList("Mandi")
+    AddToDoList("Tidur")
+    AddToDoList("Main")
+    ShowToDoList()
+    UIEditToDoList()
+    ShowToDoList()
 
 }
 
 fun UIRemoveToDoList() {
+    println("\n========= HAPUS TO DO LIST KAMU HARI INI =========")
+
+    print("Masukkan To Do List Keberapa Yang Mau DIhapus: ")
+    val Num = inputUser.nextInt()
+
+    if (Num == null){
+        println("INPUT NUMBER KAMU SALAH, PERHATIKAN KEMBALI INPUT DATA!!!")
+        return
+    }else if (Num > activity.size){
+        println("INPUT KAMU MELEBIHI BATAS DATA!!")
+    }else{
+        RemoveToDoList(Num)
+    }
+}
+
+fun TestUIRemoveToDoList() {
+    AddToDoList("Makan")
+    AddToDoList("Mandi")
+    AddToDoList("Tidur")
+    AddToDoList("Main")
+    ShowToDoList()
+    UIRemoveToDoList()
+    ShowToDoList()
 
 }
+
+fun UIAPP() {
+
+    var kondisi = true
+    while (kondisi){
+        println("APP version: 1.0")
+        println("Mengatur Harimu Dengan Lebih Tersusun")
+        println("Selamat Datang, Ada Aktivitas Apa Hari Ini?\n")
+        ShowToDoList()
+
+        println("\n1. Tambah To Do List")
+        println("2. Edit To Do List")
+        println("3. Hapus To Do List")
+        println("4. Keluar")
+
+        print("Masukkan Aksi Apa Yang Mau Dilakukan Dalam Angka: ")
+        val InputKegiatan = inputUser.nextInt()
+        inputUser.nextLine()
+
+        if (InputKegiatan == 1) {
+            UIAddToDoList()
+        } else if (InputKegiatan == 2) {
+            UIEditToDoList()
+        }else if (InputKegiatan == 3){
+            UIRemoveToDoList()
+        } else if (InputKegiatan == 4){
+            println("Aplikasi Berhenti, Terimakasih")
+            kondisi = false
+        }
+    }
+}
+
 fun main() {
     // MAIN PROGRAM
     // TestShowToDoList()
     // TestAddToDoList()
-    TestRemoveToDoList()
+    // TestRemoveToDoList()
+    // TestEditToDoList()
+    // TestUIAddToDoList()
+    // TestUIEditToDoList()
+    // TestUIRemoveToDoList()
+    UIAPP()
 }
